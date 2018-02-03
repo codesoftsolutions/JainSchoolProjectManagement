9e956b8b19261a19c607ba01c44071d321fa7191
codesoftsolutions <solutions.codesoft@gmail.com>
2018-02-03 10:34:47 +0530

    Add sql connection in create user class
    Add sql connection in create user class

--- schoolmanagementdcmjain/Master/CreateUser.cs
+++ schoolmanagementdcmjain/Master/CreateUser.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using schoolmanagementdcmjain.Master.Models;
+using MySql.Data.MySqlClient;
+using schoolmanagementdcmjain.Database;
 
 namespace schoolmanagementdcmjain.Master
 {
@@ -26,6 +28,9 @@ namespace schoolmanagementdcmjain.Master
             createUser.password = password.Text;
             createUser.name = name.Text;
             createUser.designation = designation.Text;
+            Connection connection = new Connection();
+            MySqlConnection mySqlConnection = connection.getConnection();
+
         }
     }
 }
